c2ad48e23c6f4cbefdb9a99e38b7db9426f8f982
publicMoeller <johannes.moeller@posteo.net>
2024-02-26 21:33:33 +0100

    Solved 044 Pentagon Numbers

---
+++ src/Tests/Problems/Problem044Test.java
@@ -0,0 +1,12 @@
+package Problems;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class Problem044Test {
+
+    @Test
+    void Problem044Solution() {
+        Assertions.assertEquals(Problem044.solve(), 5482660);
+    }
+}
